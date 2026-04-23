b495e9645914e606327856f268db869ba857a92d
Sarvesh Arunkumar <sarvesharunkumar2006@gmail.com>
2026-04-23 21:17:43 +0530

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        civic: {
+          saffron: '#FF9933',
+          white: '#FFFFFF',
+          green: '#128807',
+          navy: '#000080',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
